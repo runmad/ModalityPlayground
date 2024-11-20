c503114dd3e97ba33a01b61a2065799758ed5b1f
runmad <runemadsen@mac.com>
2024-11-19 19:00:23 -0800

    Initial Commit

---
+++ Modality/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Modality
+//
+//  Created by Rune Madsen on 11/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Modality/ModalityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalityApp.swift
+//  Modality
+//
+//  Created by Rune Madsen on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ModalityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
